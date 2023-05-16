545547d8c0df2625815eed26bb8752a12a90c1aa
Dan97O <dan97-oddo@live.it>
2023-05-16 15:52:52 +0200

    add css bootstrap

--- index.php
+++ index.php
@@ -28,14 +28,16 @@ require_once __DIR__ . "/db.php";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- cdn bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <title>Movies</title>
 </head>
 
-<body>
-  <main id="app_main">
-    <div class="container">
-      <h1>Lista Film</h1>
-      <ul>
+<body class="bg-black text-light">
+  <main>
+    <div class="container mt-5">
+      <h1 class="text-center">Lista Film</h1>
+      <ul class="mt-5">
         <?php foreach ($movies as $movie): ?>
           <li>
             <h2><?=$movie->get_name()?></h2>
@@ -47,7 +49,6 @@ require_once __DIR__ . "/db.php";
     <!-- /.container -->
   </main>
   <!-- /#app_main -->
-
 </body>
 
 </html>
